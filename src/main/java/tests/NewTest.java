8fea50c1096f721d0ab52eb250d96eaf8b670c10
Vu Pham Van <vanvupham1994@gmail.com>
2019-10-08 22:07:25 +0700

    Initial commit 11

--- src/main/java/tests/NewTest.java
+++ src/main/java/tests/NewTest.java
@@ -1,31 +1,31 @@
 package tests;
-
+ 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
-
+ 
 public class NewTest {
-	
-	public WebDriver driver;
-	
+ 
+ public WebDriver driver;
+ 
   @Test
   public void openMyBlog() {
-	driver.get("http://thongtinnhansu.viettel.vn/");
+ driver.get("https://www.softwaretestingmaterial.com/");
   }
   
   @BeforeClass
   public void beforeClass() {
-	  
-	  System.setProperty("webdriver.gecko.driver", "E:\\geckodriver.exe");
-	  driver = new FirefoxDriver();
-	  
+   
+   System.setProperty("webdriver.gecko.driver", "E:\\geckodriver.exe");
+   driver = new FirefoxDriver();
+   
   }
-
+ 
   @AfterClass
   public void afterClass() {
-	  driver.quit();
+   driver.quit();
   }
-
+ 
 }
