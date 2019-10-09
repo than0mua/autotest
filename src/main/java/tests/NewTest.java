9b33c57da08721e40cc013e87b6dd144dcf4e584
Vu Pham Van <vanvupham1994@gmail.com>
2019-10-10 02:01:41 +0700

    test bo driver setup

--- src/main/java/tests/NewTest.java
+++ src/main/java/tests/NewTest.java
@@ -18,7 +18,7 @@ public class NewTest {
   @BeforeClass
   public void beforeClass() {
    
-   System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+   //System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
    driver = new ChromeDriver();
    
   }
